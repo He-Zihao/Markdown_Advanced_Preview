3e8508f70b23f96e7af56e0d016c1c36b729ef91
ZH.H <85163803+He-Zihao@users.noreply.github.com>
2026-08-23 01:12:55 +1000

    Add files via upload

---
+++ js/mathjax4_config.js
@@ -0,0 +1,10 @@
+// MathJax 4 configuration for Markdown Advanced Preview.
+// This file must be loaded before tex-mml-chtml.js.
+window.MathJax = {
+  tex: {
+    tags: 'ams'
+  },
+  options: {
+    enableMenu: true
+  }
+};
